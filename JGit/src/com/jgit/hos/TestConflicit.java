88b2eaf8adfd4e2a83d5eb208ba3a4bed33b5cc3
Hosam Kandel <hossam20052@hotmail.com>
2019-11-05 15:43:23 +0400

    Update TestConflicit.java

--- JGit/src/com/jgit/hos/TestConflicit.java
+++ JGit/src/com/jgit/hos/TestConflicit.java
@@ -1,12 +1,4 @@
 package com.jgit.hos;
 
-Sysotem,out.dsdsds
-Account Number      10288558132001
-Account Name(s)     HOSAM KANDEL MOHAMED IBRAHIM
-HSBC cell center
-+202 19007/27281300
-+202 35359100
-
-
-
-*#*#7378423#*#*
+public class TestConflicit
+{}
